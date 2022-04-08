142dead3d8f76d1f93e588782b4785f93b378443
ali taheri <85300055+itstaheri@users.noreply.github.com>
2022-04-08 15:31:22 +0430

    complate ArticleComment Model

--- Hofre/AM.Domain/ArticleCommentAgg/ArticleCommentModel.cs
+++ Hofre/AM.Domain/ArticleCommentAgg/ArticleCommentModel.cs
@@ -10,10 +10,21 @@ namespace AM.Domain.ArticleCommentAgg
 {
     public class ArticleCommentModel : BaseEntity
     {
+        public ArticleCommentModel(string text, long articleId, long userId)
+        {
+            Text = text;
+            ArticleId = articleId;
+            UserId = userId;
+            IsActive = false;
+        }
+
+        public void Active()=>IsActive = true;
+
         public string Text { get; private set; }
         public long ArticleId { get; private set; }
         public ArticleModel article { get; private set; }
         public long UserId { get; private set; }
+        public bool IsActive { get; private set; }
 
 
     }
